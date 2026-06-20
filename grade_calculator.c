a0c911ff2728c0a68a1b5c4fc347be18a389be20
Takiya Tabassum <takiyatabassum2k20@gmail.com>
2026-06-20 12:36:48 +0600

    grade_calculator.c

--- grade_calculator.c
+++ grade_calculator.c
@@ -1,16 +1,26 @@
-#include<stdio.h>
-int main(){
-float mark;
-printf("Welcome to GPA calculator.\nEnter your marks:\n");
-scanf("%f", &mark);
-if(mark>90 && mark<=100){
-    printf("Congratulation!!! You got A grade");}
-    
-    else if(mark>75 && mark<=90){printf("You got B grade");}
-    else if(mark>60 && mark<=75){printf("You got C grade");}
-    else if(mark>30 && mark<=60){printf("You got D grade");}
-    else {printf("You are failed. Better luck next time");}
+#include <stdio.h>
 
+int main() {
+    float mark;
+
+    printf("Welcome to GPA calculator.\nEnter your marks:\n");
+    scanf("%f", &mark);
+
+    if (mark < 0 || mark > 100)
+        printf("Invalid marks!");
+    else if (mark >= 90)
+        printf("Congratulations!!! You got A grade");
+    else if (mark >= 75)
+        printf("You got B grade");
+    else if (mark >= 60)
+        printf("You got C grade");
+    else if (mark >= 30)
+        printf("You got D grade");
+    else
+        printf("You are failed. Better luck next time");
+
+    return 0;
+}
 
 
 
